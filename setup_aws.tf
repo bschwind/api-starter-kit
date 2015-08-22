75435af99f097af4359ee68420336345029b9618
Brian Schwind <brianmschwind@gmail.com>
2015-08-23 03:32:31 +0900

    Testing the removal of an AWS resource

--- setup_aws.tf
+++ setup_aws.tf
@@ -4,11 +4,6 @@ provider "aws" {
     region = "${var.region}"
 }
 
-resource "aws_s3_bucket" "bucket" {
-    bucket = "${var.s3_bucket}"
-    acl = "private"
-}
-
 resource "aws_instance" "api-starter-kit-dev" {
     ami = "ami-936d9d93"
     instance_type = "t2.micro"
