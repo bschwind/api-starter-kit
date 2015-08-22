8a543922e8cc5f8552e6867a246d1ce1e359f9ba
Brian Schwind <brianmschwind@gmail.com>
2015-08-22 22:49:37 +0900

    Setup variables properly

--- setup_aws.tf
+++ setup_aws.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
-    region = "ap-northeast-1"
+    region = "${var.region}"
 }
 
 resource "aws_instance" "api-starter-kit-dev" {
