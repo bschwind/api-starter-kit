fbb36fa4ff99282331b0acd347ff0fe4714d29fa
Brian Schwind <brianmschwind@gmail.com>
2015-08-31 23:52:27 +0900

    Add support for an SSH deploy key

--- setup_aws.tf
+++ setup_aws.tf
@@ -4,6 +4,11 @@ provider "aws" {
     region = "${var.region}"
 }
 
+resource "aws_key_pair" "deployer" {
+  key_name = "${public_deploy_key_name}"
+  public_key = "${var.public_deploy_key}"
+}
+
 resource "aws_s3_bucket" "bucket" {
     bucket = "${var.s3_bucket}"
     acl = "private"
@@ -12,7 +17,7 @@ resource "aws_s3_bucket" "bucket" {
 resource "aws_instance" "api-starter-kit-dev" {
     ami = "ami-936d9d93"
     instance_type = "t2.micro"
-    key_name = "tokyo-free"
+    key_name = "${aws_key_pair.deployer.key_name}"
 
     provisioner "local-exec" {
         command = "ansible --version"
--- variables.tf
+++ variables.tf
@@ -1,5 +1,7 @@
 variable "access_key" {}
 variable "secret_key" {}
+variable "public_deploy_key" {}
+variable "public_deploy_key_name" {}
 variable "region" {
     default = "ap-northeast-1"
 }
