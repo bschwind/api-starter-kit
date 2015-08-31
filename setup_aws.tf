97ee44e9591931dd626bd51d9c58bc6007fa7b80
Brian Schwind <brianmschwind@gmail.com>
2015-08-31 23:11:59 +0900

    Maybe if the documentation were better

--- setup_aws.tf
+++ setup_aws.tf
@@ -12,7 +12,7 @@ resource "aws_s3_bucket" "bucket" {
 resource "aws_instance" "api-starter-kit-dev" {
     ami = "ami-936d9d93"
     instance_type = "t2.micro"
-    key-name = "api-starter-kit-dev"
+    key-name = "tokyo-free"
 
     provisioner "local-exec" {
         command = "ansible --version"
